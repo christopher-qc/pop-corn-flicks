0f1bcc266d165c9d6d58f0d47bfe05f4693b70df
Christopher <jazielquine@gmail.com>
2024-05-19 13:00:46 -0500

    second_commit

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,18 +1,22 @@
 import '../styles/Info.css'
 
 const Info = ({ info, genres }) => {
-  const { title, overview, release_date } = info
+  const { title, overview, release_date, vote_average } = info
   const date = new Date(release_date);
   const year = date.getFullYear();
-
+  const vote = vote_average.toFixed(1)
 
   return (
     <div className="info">
       <h1 className='title'>{title}</h1>
-      <p>{year}</p>
       <p className='genre'>{info.genre_ids.map(id => genres[id]).join(' | ')}</p>
+      <p>{year}</p>
+      <p style={{ display: 'flex', alignItems: 'center' }}>
+        <i className="material-icons" style={{ fontSize: '18px', marginRight: '5px', color: 'yellow'}}>star</i>
+        {vote} / 10
+      </p>
       <div className="description-container">
-        <p className='description'>{overview}</p>
+        <p className='description'>{overview || 'Movie synopsis not available'}</p>
       </div>
     </div>
     )
